6132266035c771637712c87be203f041c5c7ed55
FrancescoCapriolo <francesco.capriolo92@gmail.com>
2022-02-22 17:22:43 +0100

    js completato

--- js/main.js
+++ js/main.js
@@ -1,35 +1,28 @@
-/* function myFunction() {
-    let bill = parseFloat(prompt("per favore inserisci i kilometri che devi percorrere");
+function myFunction() {
+    let bill = parseInt(prompt("per favore inserisci i kilometri che devi percorrere"));
+    console.log(bill);
 
-        if (bill != null) {
-            
-        }
+    console.log(bill * 0.21);
+    bill = (bill * 0.21);
 
+    let age = parseInt(prompt("per favore inserisci la tua età"));
+    console.log(age);
+
+    if ((isNaN(bill) || (isNaN(age)))) {
         document.getElementById("prezzo").innerHTML =
-        bill ;
-    } */
-let bill = parseInt(prompt("per favore inserisci i kilometri che devi percorrere"));
-console.log(bill);
-
-console.log(bill * 0.21);
-bill = (bill * 0.21);
-
-let age = parseInt(prompt("per favore inserisci la tua età"));
-console.log(age);
-
-if (age <= 18) {
-    console.log(bill - (bill * (20 / 100)));
-    bill = Math.round(bill - (bill * (20 / 100)));
-} else if (age >= 65) {
-    console.log(bill - (bill * (40 / 100)));
-    bill = Math.round(bill - (bill * (40 / 100)));
-} else {
-    console.log(bill)
-}
-
-document.getElementById("prezzo").innerHTML = Math.round(bill) + "€";
-
-if ((isNaN(bill) || (isNaN(age)))) {
-    document.getElementById("prezzo").innerHTML =
-        "Hai scelto un numero non valido";
+            "Hai scelto un numero non valido";
+    } else {
+        if (age <= 18) {
+            console.log(bill - (bill * (20 / 100)));
+            bill = (bill - (bill * (20 / 100))).toFixed(2);
+            console.log(bill)
+        } else if (age >= 65) {
+            console.log(bill - (bill * (40 / 100)));
+            bill = (bill - (bill * (40 / 100))).toFixed(2);
+            console.log(bill)
+        } else {
+            console.log(bill)
+        }
+        document.getElementById("prezzo").innerHTML = (bill) + "€";
+    }
 }
